493ab6403484473ea3bdd6be7b8732e1c1c8da3e
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-07-28 18:31:47 +0700

    adding ID to react

--- src/components/pokemonPage/Pokemon.jsx
+++ src/components/pokemonPage/Pokemon.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Pokemon = () => {
+
+const [result, setResult] = useState([])
+const [filtering, setFiltering] = useState('')
+
+useEffect(() => {
+  fetch('https://pokeapi.co/api/v2/pokemon')
+  .then((response) => {
+    // console.log(response.status);
+    return response.json()
+  })
+  .then((data) => {
+    // console.log(data);
+    setResult(data.results)
+  })
+  .catch((error) => {
+    console.log('ini ada error');
+  })
+},[])
+
+  function handleFilter(params) {
+    setFiltering(params.target.value)
+  }
+
+  const pokemonUrl = result.map((pokeUrl) =>{
+    return pokeUrl.url.split('/')
+  })
+
+  console.log(pokemonUrl);
+
+
+  const filterPokemon = result.filter((dataPokemon)=> {
+    return dataPokemon.name.includes(filtering)
+  })
+
+  // console.log(result);
+
+  
   return (
-    <div>Pokemon</div>
+    <>
+      <div>Pokemon</div>  
+
+      <input type="text" onChange={handleFilter} />   
+      <br />
+      hasil : {filterPokemon.map((mapPokemon) => {
+        return <div key={mapPokemon.name}>{mapPokemon.name} . {mapPokemon.url.split('/')[6]}</div>
+
+      })} 
+
+ 
+
+
+      {/* {result.map((dataPokemon, index) => {
+          return <div key={dataPokemon.name}>{index+1}. {dataPokemon.name}</div>     
+      })} */}
+    </>
   )
 }
 
