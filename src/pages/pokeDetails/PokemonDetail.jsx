ae20725897daecd1f0cc5cf7432c214136b2d140
Bimo Dwien Prabowo <bimodwien2@gmail.com>
2023-08-23 15:02:13 +0700

    content-detail, tinggal yang back to home saja

--- src/pages/pokeDetails/PokemonDetail.jsx
+++ src/pages/pokeDetails/PokemonDetail.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { useFetchDetail } from "../../helper/hooks";
 
+import "./detail.css";
+
 const PokemonDetail = () => {
   const parameter = useParams();
 
@@ -11,28 +13,36 @@ const PokemonDetail = () => {
   });
   return (
     <>
-      <img
-        src={dataDetailPokemon.sprites?.other["official-artwork"].front_default}
-        alt={dataDetailPokemon.name}
-      />
-      <div>Name = {dataDetailPokemon.name}</div>
-      {/* kenapa dikasih ?, karena ada kemungkinan dataDetail.stats bisa undefined, kalo object ga perlu dikasih ? */}
-      <div>
-        {dataDetailPokemon.stats?.map?.((dataStats) => {
-          return (
-            <div key={dataStats.stat.name}>
-              {dataStats.stat.name} = {dataStats.base_stat}
+      <div className="content-body">
+        <div className="details-name">{dataDetailPokemon.name}</div>
+        <div className="content-details">
+          <div className="content-details-photo">
+            <img src={dataDetailPokemon.sprites?.other["official-artwork"].front_default} alt={dataDetailPokemon.name} className="detail-images"/>
+          </div>
+          {/* kenapa dikasih ?, karena ada kemungkinan dataDetail.stats bisa undefined, kalo object ga perlu dikasih ? */}
+          <div className="content-details-status">
+            <div>
+              {dataDetailPokemon.stats?.map?.((dataStats) => {
+                return (
+                  <div key={dataStats.stat.name}>
+                    {dataStats.stat.name} = {dataStats.base_stat}
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
+            <div className="content-details-tag">
+              {dataDetailPokemon.types?.map((dataTypes) => {
+                return (
+                  <div key={dataTypes.slot} className="type-details">
+                    {dataTypes.type.name}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <Link to="/">back</Link>
       </div>
-      <div>
-        {dataDetailPokemon.types?.map((dataTypes) => {
-          return <div key={dataTypes.slot}>Type = {dataTypes.type.name}</div>;
-        })}
-      </div>
-
-      <Link to="/">back</Link>
     </>
   );
 };
